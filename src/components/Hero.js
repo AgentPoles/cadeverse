56b26ee273aacc9c4207a23d62bc944681fe2781
Agentpoles <pauloams16@gmail.com>
2022-02-15 15:25:00 -0800

    update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,7 +10,7 @@ import pg from "../images/MaleIran.png";
 
 export default function Example() {
   return (
-    <div className="relative bg-white overflow-hidden">
+    <div className="relative bg-white overflowx-hidden">
       <div className="pt-16 pb-80 sm:pt-24 sm:pb-40 lg:pt-40 lg:pb-48">
         <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 sm:static">
           <div className="sm:max-w-lg">
@@ -27,9 +27,9 @@ export default function Example() {
               {/* Decorative image grid */}
               <div
                 aria-hidden="true"
-                className="pointer-events-none lg:absolute lg:inset-y-0 lg:max-w-7xl lg:mx-auto lg:w-full"
+                className="pointer-events-none lg:absolute lg:max-w-7xl lg:mx-auto lg:w-full"
               >
-                <div className="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
+                <div className="absolute transform sm:left-1/2 sm:top-3 sm:translate-x-8 sm:translate-y-1/3 md:left-1/2 md:top-3 md:translate-x-8 md:translate-y-1/3 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/3 lg:translate-x-6">
                   <div className="flex items-center space-x-6 lg:space-x-8">
                     <div className="flex-shrink-0 grid grid-cols-1 gap-y-6 lg:gap-y-8">
                       <div className="w-44 h-64 rounded-lg overflow-hidden sm:opacity-0 lg:opacity-100">
@@ -48,7 +48,7 @@ export default function Example() {
                       </div>
                     </div>
                     <div className="flex-shrink-0 grid grid-cols-1 gap-y-6 lg:gap-y-8">
-                      <div className="w-44 h-64 rounded-lg overflow-hidden">
+                      <div className="w-44 h-64 rounded-lg overflow-hidden sm:opacity-0 lg:opacity-100">
                         <img
                           src={pc}
                           alt=""
@@ -90,14 +90,12 @@ export default function Example() {
                 </div>
               </div>
 
-              <div class="flex flex-row space-x-1 ">
+              <div class="flex flex-row space-x-3 ">
                 <div>
                   {" "}
                   <a
                     href="#"
-                    className="inline-block text-center bg-slate-600 border border-transparent rounded-md py-3 px-1 font-small text-white hover:bg-slate-500"
-                    bg-gradient-to-r
-                    from-indigo-500
+                    className="inline-block text-center bg-gradient-to-r from-pink-600 to-pink-500 border border-transparent rounded-md py-3 px-2 font-normal shadow-md text-sm  text-white hover:bg-slate-500"
                   >
                     Obtain Identity
                   </a>
@@ -106,7 +104,7 @@ export default function Example() {
                   {" "}
                   <a
                     href="#"
-                    className="inline-block text-center bg-indigo-600 border border-transparent rounded-md py-3 px-8 font-medium text-white hover:bg-indigo-700"
+                    className="inline-block text-center bg-gradient-to-r from-pink-500 to-orange-300 border border-transparent rounded-md py-3 px-2 font-normal shadow-md text-sm  text-white hover:bg-slate-500"
                   >
                     Borrow Identity
                   </a>
@@ -115,7 +113,7 @@ export default function Example() {
                   {" "}
                   <a
                     href="#"
-                    className="inline-block text-center bg-indigo-600 border border-transparent rounded-md py-3 px-8 font-medium text-white hover:bg-indigo-700"
+                    className="inline-block text-center  bg-gradient-to-r from-rose-400 to-pink-600 border-transparent rounded-md py-3 px-2 font-normal shadow-md text-sm  text-white hover:bg-slate-500"
                   >
                     Upload Identity
                   </a>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,33 @@
 import React from "react";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import MakeNft from "../components/MakeNft";
 import Masonry from "../components/Masonry";
+import Finish from "../components/Finish";
+import { useState } from "react";
+import { create } from "ipfs-http-client";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import storage from "../storage/firebasefile";
+import Identities from "../components/Identities";
+import Herob from "../components/Herob";
+import Footer from "../components/Footer";
+
+// require("dotenv").config();
 
 const Home = () => {
+  const [fileUrl, updateFileUrl] = useState("");
+  const [nft, setNft] = useState({
+    image: "",
+    name: "",
+    description: "",
+    image_name: "",
+  });
+
+  const cliente = create("https://ipfs.infura.io:5001/api/v0");
+
   return (
-    <div>
-      <Header></Header>
-      <Hero></Hero>
-      <Masonry />
+    <div className="container">
+      <Herob></Herob>
     </div>
   );
 };
